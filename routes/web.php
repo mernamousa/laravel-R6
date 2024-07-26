9162eea2e6883bce77ac50ffcfc28e9c140cbd67
mernamousa <mernamousa209@gmail.com>
2024-07-26 15:17:30 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,11 @@ Route::get('create', [ClassController::class,'create'])->name('class.create');
 Route::post('store', [ClassController::class,'store'])->name('class.store');
 Route::get('index', [ClassController::class,'index'])->name('class.index');
 Route::get('edit/{id}', [ClassController::class,'edit'])->name('class.edit');
-Route::put('update/{id}', [ClassController::class,'update'])->name('class.update');
+//Route::put('update/{id}', [ClassController::class,'update'])->name('class.update');
+//Route::get('show/{id}', [ClassController::class,'show'])->name('cars.show');
+//Route::get('delete/{id}', [ClassController::class,'destroy'])->name('cars.destroy');
+//Route::get('showDeleted', [ClassController::class,'showDeleted'])->name('cars.showDeleted');
+
 
 });
 
