7b42e769598db28f8056df2e2d7ce3524f445e57
mernamousa <mernamousa209@gmail.com>
2024-07-28 21:41:42 +0300

    make validation

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -30,15 +30,14 @@ class CarController extends Controller
      */
     public function store(Request $request)
     {
-       //dd($request);
-       $data =[
-          'carTitle' => $request->carTitle,
-          'price' => $request->price,
-          'description'=> $request->description ,
-          'published'=> $request->has('published'),
-         // 'published'=> isset($request->published),
+        $data =$request->validate([
+          'carTitle' =>'required|string',
+          'description' =>'required|string|max:1000',
+          'price'=>'required|decimal:1',
+        ]);
+        $data['published']=$request->has('published');
 
-       ];
+       //dd($data);
   
           Car::create($data);
   
